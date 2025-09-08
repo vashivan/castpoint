8a8d62637aefea3cee9751e4fbc2e6deb65279aa
Ivan Vashchuk <vaniavaschuk@gmail.com>
2025-09-08 18:43:44 +0900

    Fixed page

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,19 +1,29 @@
-'use client'
-
-// import MainLayout from '../../layouts/MainLayout';
-import ResetPassword from '../../components/layout/ResetPassword';
-import React from 'react';
-
+// app/reset-password/page.tsx
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
+import ResetForm from "../../components/layout/ResetPassword"
 
-export default function ResetPasswordPage({
-  searchParams,
-}: {
-  searchParams: { token?: string; email?: string };
+type SP = Record<string, string | string[] | undefined>;
+
+function getVal(sp: SP, key: string) {
+  const v = sp[key];
+  return Array.isArray(v) ? v[0] : v ?? '';
+}
+
+export default async function ResetPasswordPage(props: {
+  searchParams?: Promise<SP> | SP;
 }) {
-  const token = typeof searchParams?.token === 'string' ? searchParams.token : '';
-  const email = typeof searchParams?.email === 'string' ? searchParams.email : '';
-  return <ResetPassword token={token} email={email} />;
+  function isPromise(obj: unknown): obj is Promise<SP> {
+    return !!obj && typeof (obj as Promise<SP>).then === 'function';
+  }
+
+  const sp = (props.searchParams && isPromise(props.searchParams))
+    ? await props.searchParams
+    : ((props.searchParams as SP) ?? {});
+
+  const token = getVal(sp, 'token');
+  const email = getVal(sp, 'email');
+
+  return <ResetForm token={token} email={email} />;
 }
