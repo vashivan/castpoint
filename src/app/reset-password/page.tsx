d08bb25b989c03f973da952b2a2c8f1d36fc3b1c
Ivan Vashchuk <vaniavaschuk@gmail.com>
2025-09-08 16:36:34 +0900

    Fix pages

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 
-import MainLayout from '../../layouts/MainLayout';
+// import MainLayout from '../../layouts/MainLayout';
 import ResetPassword from '../../components/layout/ResetPassword';
 import React from 'react';
 
-export default function Page() {
-  return (
-    <MainLayout>
-      <ResetPassword />
-    </MainLayout>
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
-  );
+
+export default function ResetPasswordPage({
+  searchParams,
+}: {
+  searchParams: { token?: string; email?: string };
+}) {
+  const token = typeof searchParams?.token === 'string' ? searchParams.token : '';
+  const email = typeof searchParams?.email === 'string' ? searchParams.email : '';
+  return <ResetPassword token={token} email={email} />;
 }
